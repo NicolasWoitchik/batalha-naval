81084ecde7fc5be60d6ca251aaa728835045fc1d
Nicolas <nicolas@headerinteractive.com>
2020-02-21 14:28:48 -0300

    feat: Added styled-components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import { App } from "./style";
 
-function handleEnterRoom() {
-  alert("uau");
-}
-function App() {
+function Main() {
   return (
-    <div className="App">
+    <App>
       <header className="App-header">
         <img src={logo} className="App-logo " alt="logo" />
         <p>Bem vindo</p>
         <p>Insira o código da sala abaixo ou crie uma</p>
-        <input
-          placeholder="Código da sala"
-          name="codigo-sala"
-          onChange={handleEnterRoom}
-        />
+        <input placeholder="Código da sala" name="codigo-sala" />
         <a className="App-link" href="/criar-sala" rel="noopener noreferrer">
           Criar uma Sala
         </a>
       </header>
-    </div>
+    </App>
   );
 }
 
-export default App;
+export default Main;
